60ddd97f1ee9538234e00911e4cfeda46d8cc2f6
Adrian-Marin <a18310176@live.ceti.mx>
2020-10-25 00:55:49 -0500

    2.1.3
    Registro de cambios:
     + El brillo por default ahora es 0 (cero).
     ! Los scripts fueron movidos a carpetas de acuerdo a su función.

---
+++ Menu 1.0.0/Assets/Scripts/Menu/Settings/tmpBrillo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+using Debug = UnityEngine.Debug;
+
+/*
+    Archivo de configuración del valor de brillo
+*/
+
+public class tmpBrillo : MonoBehaviour
+{
+    private TMP_Text tmp;
+    public Slider mainSlider;
+    private int sliderValue;
+
+    void Start()
+    {
+        tmp = GetComponent<TMP_Text>();
+    }
+
+    
+    void Update()
+    {
+        sliderValue = (int)Math.Round(mainSlider.value);
+        tmp.text = sliderValue.ToString();
+    }
+}
